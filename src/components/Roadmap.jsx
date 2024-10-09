cb8a88f8683455b8686aa381a9dbf3308ff6db80
zhassan2312 <zhassan.ce44ceme@student.nust.edu.pk>
2024-10-10 00:02:33 +0500

    Implementing Roadmap section

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,7 +1,7 @@
 import Button from "./Button";
 import Heading from "./Heading";
 import Section from "./Section";
-import Tagline from "./TagLine";
+import Tagline from "./Tagline";
 import { roadmap } from "../constants";
 import { check2, grid, loading1 } from "../assets";
 import { Gradient } from "./design/Roadmap";
